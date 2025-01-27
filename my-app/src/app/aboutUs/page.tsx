8f23eb9fa83f372a8ddda8a06016bd124109342e
be-wing <sin.hanari@gmail.com>
2025-01-28 04:00:28 +0900

    ページの作成

---
+++ my-app/src/app/aboutUs/page.tsx
@@ -0,0 +1,8 @@
+
+export default function AboutUs() {
+    return (
+        <main className="p-3">
+            <h1>自己紹介</h1>
+        </main>
+    )
+}
---
+++ my-app/src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+        <main className="p-3">
+            <h1>お問合せ</h1>
+        </main>
+    )
+}
